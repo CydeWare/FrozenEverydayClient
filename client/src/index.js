f7a41167deaea5144ac665a3dc3d6774807b9e73
CydeWare <71936967+CydeWare@users.noreply.github.com>
2025-03-31 23:48:29 +0700

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers/index.js";
+import { thunk } from "redux-thunk";
+import "./styles.css"
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <React.StrictMode>
+            {/* REMOVE THIS CLIENT ID AND USE ENV FILE TO FETCH IT */}
+            <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+                <App />
+            </GoogleOAuthProvider>
+        </React.StrictMode>
+    </Provider>
+);
